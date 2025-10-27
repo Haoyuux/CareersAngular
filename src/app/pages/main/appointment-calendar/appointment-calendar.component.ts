88f1bfaa477e5bae880c3074ab9513fe99c1d2bd
Haoyuux <mercadoblaise@gmail.com>
2025-10-27 10:55:48 +0800

    calendar module
    ui / static data for calendar

--- src/app/pages/main/appointment-calendar/appointment-calendar.component.ts
+++ src/app/pages/main/appointment-calendar/appointment-calendar.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-appointment-calendar',
   standalone: true,
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './appointment-calendar.component.html',
-  styleUrl: './appointment-calendar.component.scss'
+  styleUrl: './appointment-calendar.component.scss',
 })
 export class AppointmentCalendarComponent {
+  constructor() {}
 
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, interactionPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: '',
+    },
+    aspectRatio: 1.2, // controls height/width ratio
+    height: 'auto',
+    contentHeight: 'auto',
+    dayMaxEventRows: true,
+    weekends: true,
+    events: [
+      { title: 'Interview', date: '2025-10-25', classNames: ['pending'] },
+      { title: 'Rejected', date: '2025-10-26', classNames: ['not-going'] },
+      { title: 'Accepted', date: '2025-10-27', classNames: ['going'] },
+      { title: 'PMD Approved', date: '2025-10-28', classNames: ['confirmed'] },
+    ],
+  };
 }
